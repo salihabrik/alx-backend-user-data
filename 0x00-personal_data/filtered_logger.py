83cfeedc43414a431e88cb499fc49b91fb91a029
salihabrik <briksaliha@gmail.com>
2024-02-08 22:16:17 +0100

    Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-"""
-Module for log filtering using regex
+
+""" A function filter_datum that
+    returns the log message obfuscated
 """
 
-
+import logging
+from typing import List
 import re
 
+
 def filter_datum(fields: List[str], redaction: str,
-                message: str, separator: str) -> str:
+                 message: str, separator: str) -> str:
     """ Return: the log message obfuscated """
     return(re.sub(rf"({'|'.join(fields)})=.*?{separator}",
-        rf"\1={redaction}{separator}", message))
-    
+           rf"\1={redaction}{separator}", message))
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class """
@@ -23,3 +26,9 @@ class RedactingFormatter(logging.Formatter):
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format Function """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return (super().format(record))
