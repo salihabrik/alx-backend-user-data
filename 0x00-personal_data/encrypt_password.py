549e29abd9f42fdca14a403a24b20920d9b6a2f4
salihabrik <briksaliha@gmail.com>
2024-02-08 23:15:39 +0100

    encrypt-password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3
+""" password encryption module
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Implement a hash_password function that expects 
+    one string argument name password and returns a salted, 
+    """
+    encrypt = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return encrypt
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Implement an is_valid function that expects 2 arguments and returns a boolean.
+    """
+    validity = bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return validity#!/usr/bin/env python3
+""" password encryption module
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Implement a hash_password function that expects 
+    one string argument name password and returns a salted, 
+    """
+    encrypt = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return encrypt
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Implement an is_valid function that expects 2 arguments and returns a boolean.
+    """
+    validity = bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return validity
