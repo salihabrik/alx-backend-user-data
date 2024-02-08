2efa57e0ae20a490e55b72707958afa88d92fd91
salihabrik <briksaliha@gmail.com>
2024-02-08 22:10:44 +0100

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,11 @@
 Module for log filtering using regex
 """
 
+
 import re
 
 def filter_datum(fields: List[str], redaction: str,
                 message: str, separator: str) -> str:
     """ Return: the log message obfuscated """
     return(re.sub(rf"({'|'.join(fields)})=.*?{separator}",
-           rf"\1={redaction}{separator}", message))
+        rf"\1={redaction}{separator}", message))
