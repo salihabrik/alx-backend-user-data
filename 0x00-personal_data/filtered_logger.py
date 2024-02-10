d4a5dc10a84db52837912caf2bfd496dad529531
salihabrik <briksaliha@gmail.com>
2024-02-10 22:02:58 +0100

    update filtered-logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
-""" A function filter_datum that
-    returns the log message obfuscated
+""" 0x05. Personal data
+How to implement a log filter that will obfuscate PII fields
+How to encrypt a password and check the validity
+How to authenticate to a database using environment variables
 """
 import os
 import re
