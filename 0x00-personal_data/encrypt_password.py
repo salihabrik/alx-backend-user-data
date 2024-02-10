91c6af555c9a84bf6ad4df375606115e361ec4bb
salihabrik <briksaliha@gmail.com>
2024-02-10 22:11:52 +0100

    update encrypted-password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,15 +5,15 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Implement a hash_password function that expects 
-    one string argument name password and returns a salted, 
+    """Implement a hash_password function that expects
+    one string argument name password and returns a salted,
     """
     encrypt = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return encrypt
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Implement an is_valid function that expects 2 arguments and returns a boolean.
+    """Implement an is_valid function
     """
     validity = bcrypt.checkpw(password.encode('utf-8'), hashed_password)
     return validity
