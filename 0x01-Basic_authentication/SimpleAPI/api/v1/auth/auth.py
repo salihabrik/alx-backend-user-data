ee9b0ab90707005cda8bb64a38e7524a75e97b5e
salihabrik <briksaliha@gmail.com>
2024-02-14 00:19:25 +0100

    update auth

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -1,137 +1,49 @@
 #!/usr/bin/env python3
-""" Base module
+""" Module of Authentication
 """
-from datetime import datetime
-from typing import TypeVar, List, Iterable
-from os import path
-import json
-import uuid
+from flask import request
+from typing import List, TypeVar
 
 
-TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
-DATA = {}
+class Auth:
+    """ Class to manage the API authentication """
 
-
-class Base():
-    """ Base class
-    """
-
-    def __init__(self, *args: list, **kwargs: dict):
-        """ Initialize a Base instance
-        """
-        s_class = str(self.__class__.__name__)
-        if DATA.get(s_class) is None:
-            DATA[s_class] = {}
-
-        self.id = kwargs.get('id', str(uuid.uuid4()))
-        if kwargs.get('created_at') is not None:
-            self.created_at = datetime.strptime(kwargs.get('created_at'),
-                                                TIMESTAMP_FORMAT)
-        else:
-            self.created_at = datetime.utcnow()
-        if kwargs.get('updated_at') is not None:
-            self.updated_at = datetime.strptime(kwargs.get('updated_at'),
-                                                TIMESTAMP_FORMAT)
-        else:
-            self.updated_at = datetime.utcnow()
-
-    def __eq__(self, other: TypeVar('Base')) -> bool:
-        """ Equality
-        """
-        if type(self) != type(other):
-            return False
-        if not isinstance(self, Base):
-            return False
-        return (self.id == other.id)
-
-    def to_json(self, for_serialization: bool = False) -> dict:
-        """ Convert the object a JSON dictionary
-        """
-        result = {}
-        for key, value in self.__dict__.items():
-            if not for_serialization and key[0] == '_':
-                continue
-            if type(value) is datetime:
-                result[key] = value.strftime(TIMESTAMP_FORMAT)
-            else:
-                result[key] = value
-        return result
-
-    @classmethod
-    def load_from_file(cls):
-        """ Load all objects from file
-        """
-        s_class = cls.__name__
-        file_path = ".db_{}.json".format(s_class)
-        DATA[s_class] = {}
-        if not path.exists(file_path):
-            return
-
-        with open(file_path, 'r') as f:
-            objs_json = json.load(f)
-            for obj_id, obj_json in objs_json.items():
-                DATA[s_class][obj_id] = cls(**obj_json)
-
-    @classmethod
-    def save_to_file(cls):
-        """ Save all objects to file
-        """
-        s_class = cls.__name__
-        file_path = ".db_{}.json".format(s_class)
-        objs_json = {}
-        for obj_id, obj in DATA[s_class].items():
-            objs_json[obj_id] = obj.to_json(True)
-
-        with open(file_path, 'w') as f:
-            json.dump(objs_json, f)
-
-    def save(self):
-        """ Save current object
-        """
-        s_class = self.__class__.__name__
-        self.updated_at = datetime.utcnow()
-        DATA[s_class][self.id] = self
-        self.__class__.save_to_file()
-
-    def remove(self):
-        """ Remove object
-        """
-        s_class = self.__class__.__name__
-        if DATA[s_class].get(self.id) is not None:
-            del DATA[s_class][self.id]
-            self.__class__.save_to_file()
-
-    @classmethod
-    def count(cls) -> int:
-        """ Count all objects
-        """
-        s_class = cls.__name__
-        return len(DATA[s_class].keys())
-
-    @classmethod
-    def all(cls) -> Iterable[TypeVar('Base')]:
-        """ Return all objects
-        """
-        return cls.search()
-
-    @classmethod
-    def get(cls, id: str) -> TypeVar('Base'):
-        """ Return one object by ID
-        """
-        s_class = cls.__name__
-        return DATA[s_class].get(id)
-
-    @classmethod
-    def search(cls, attributes: dict = {}) -> List[TypeVar('Base')]:
-        """ Search all objects with matching attributes
-        """
-        s_class = cls.__name__
-        def _search(obj):
-            if len(attributes) == 0:
-                return True
-            for k, v in attributes.items():
-                if (getattr(obj, k) != v):
-                    return False
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method for validating if endpoint requires auth """
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        
-        return list(filter(_search, DATA[s_class].values()))
+
+        l_path = len(path)
+        if l_path == 0:
+            return True
+
+        slash_path = True if path[l_path - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Method that handles authorization header """
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Validates current user """
+        return None
