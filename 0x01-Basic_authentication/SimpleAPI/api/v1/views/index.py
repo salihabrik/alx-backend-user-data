07439f1bab21992665af338250b4acd96d14926b
salihabrik <briksaliha@gmail.com>
2024-02-14 00:21:15 +0100

    update views

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -25,18 +25,20 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> None:
-      """GET /api/v1/unauthorized
-      Return:
-      - Unauthorized error.
-      """
-      return abort(401)
 
-@app_views.route('/forbidden/', strict_slashes=False)
-def forbidden() -> None:
-      """GET /api/v1/forbidden
-      Return:
-      - forbidden error.
-      """
-      return abort(403)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - raises a 401 error by using abort
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - raises a 403 error by using abort
+    """
+    abort(403)
