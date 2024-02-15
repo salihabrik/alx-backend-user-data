fb971b19bf8494e959630e62fd671a6dc14f3024
salihabrik <briksaliha@gmail.com>
2024-02-15 21:24:23 +0100

    Sessions in database

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -13,45 +13,46 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = os.getenv("AUTH_TYPE")
-if AUTH_TYPE == "auth":
-    from api.v1.auth.auth import Auth
+
+auth_type = getenv("AUTH_TYPE", None)
+if auth_type == 'auth':
+    from .auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
+elif auth_type == 'basic_auth':
+    from .auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif AUTH_TYPE == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
+elif auth_type == 'session_auth':
+    from .auth.session_auth import SessionAuth
     auth = SessionAuth()
-elif AUTH_TYPE == "session_exp_auth":
-    from api.v1.auth.session_exp_auth import SessionExpAuth
+elif auth_type == 'session_exp_auth':
+    from .auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
-elif AUTH_TYPE == "session_db_auth":
-    from api.v1.auth.session_db_auth import SessionDBAuth
+elif auth_type == 'session_db_auth':
+    from .auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
 
 
 @app.before_request
-def bef_req():
-    """
-    Filter each request before it's handled by the proper route
-    """
-    if auth is None:
-        pass
-    else:
-        setattr(request, "current_user", auth.current_user(request))
-        excluded = [
+def before_request():
+    """ Filtering of each request """
+    if auth:
+        paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
             '/api/v1/auth_session/login/'
-        ]
-        if auth.require_auth(request.path, excluded):
+            ]
+        if auth.require_auth(request.path, paths):
+            user = auth.current_user(request)
+            authorization = auth.authorization_header(request)
             cookie = auth.session_cookie(request)
-            if auth.authorization_header(request) is None and cookie is None:
-                abort(401, description="Unauthorized")
-            if auth.current_user(request) is None:
-                abort(403, description="Forbidden")
+
+            if not authorization and not cookie:
+                abort(401)
+            if not user:
+                abort(403)
+
+            request.current_user = user
 
 
 @app.errorhandler(404)
@@ -63,14 +64,14 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Request unauthorized handler
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Request unauthorized handler
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,59 +1,54 @@
 #!/usr/bin/env python3
-"""
-Define class SessionDButh
-"""
+""" Manage the API authentication """
+from datetime import datetime, timedelta
 from .session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 
 
 class SessionDBAuth(SessionExpAuth):
-    """
-    Definition of SessionDBAuth class that persists session data
-    in a database
-    """
-
-    def create_session(self, user_id=None):
-        """
-        Create a Session ID for a user_id
-        Args:
-           user_id (str): user id
+    """ Session DB Auth Class """
+    def create_session(self, user_id=None) -> str:
+        """ Creates and stores new instance of UserSession
+            and returns the Session ID
         """
         session_id = super().create_session(user_id)
-        if not session_id:
-            return None
-        kw = {
-            "user_id": user_id,
-            "session_id": session_id
-        }
-        user = UserSession(**kw)
-        user.save()
-        return session_id
+        if isinstance(session_id, str):
+            user_session = UserSession(user_id=user_id, session_id=session_id)
+            user_session.save()
+            return (session_id)
+        return (None)
 
-    def user_id_for_session_id(self, session_id=None):
+    def user_id_for_session_id(self, session_id=None) -> str:
+        """ Returns the User ID by requesting UserSession
+            in the database based on session_id
         """
-        Returns a user ID based on a session ID
-        Args:
-            session_id (str): session ID
-        Return:
-            user id or None if session_id is None or not a string
-        """
-        user_id = UserSession.search({"session_id": session_id})
-        if user_id:
-            return user_id
-        return None
+        try:
+            sessions = UserSession.search({"session_id": session_id})
+        except Exception:
+            return (None)
 
-    def destroy_session(self, request=None):
+        if not len(sessions):
+            return (None)
+
+        current_time = datetime.utcnow()
+        created_at = sessions[0].created_at
+        expire = created_at + timedelta(seconds=self.session_duration)
+        if expire < current_time:
+            return (None)
+        return (sessions[0].user_id)
+
+    def destroy_session(self, request=None) -> bool:
+        """ Destroys the UserSession based on the Session ID
+            from the request cookie
         """
-        Destroy a UserSession instance based on a
-        Session ID from a request cookie
-        """
-        if request is None:
-            return False
         session_id = self.session_cookie(request)
-        if not session_id:
-            return False
-        user_session = UserSession.search({"session_id": session_id})
-        if user_session:
-            user_session[0].remove()
-            return True
-        return False
+        try:
+            sessions = UserSession.search({"session_id": session_id})
+        except Exception:
+            return (False)
+
+        if len(sessions):
+            sessions[0].remove()
+            return (True)
+
+        return (False)
