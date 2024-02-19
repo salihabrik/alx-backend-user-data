1b28ad9ff387818f8f6004a2149aed9ba60ae363
salihabrik <briksaliha@gmail.com>
2024-02-19 22:29:45 +0100

    Create  User

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,58 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a new user to the database
+
+        Args:
+            email (str): User's email
+            hashed_password (str): User's hashed password
+
+        Returns:
+            User: User object representing the added user
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+# Example usage in main.py
+if __name__ == "__main__":
+    from db import DB
+    from user import User
+
+    my_db = DB()
+
+    user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+    print(user_1.id)
+
+    user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+    print(user_2.id)
